83b13a09f66cfcda4bbc363755c0a62532cdc71f
Nick <nick@Sabrinas-MacBook-Pro-4.local>
2022-04-17 06:11:01 -0400

    Started 31.2 practice

--- async_promises/app.js
+++ async_promises/app.js
@@ -1,10 +1,12 @@
 
-// Part 1 
+// Part 1 Numbers
+
+// Gets a single fact about the number seven
 const numPromise = axios.get(`http://numbersapi.com/7/trivia?json`);
 numPromise
 .then(data => console.log(data));
 
-
+// Used to show the facts of random numbers
 const body = document.querySelector('body')
 const ul = document.createElement('ul')
 
@@ -20,13 +22,13 @@ Promise.all(numsPromises)
 .then(arr => (arr.forEach(p => {
     let li = document.createElement('li')
     li.innerHTML = `<strong style='font-size: 20px;'>${p.data.number}:</strong> ${p.data.text}`
-
     ul.appendChild(li) 
 })))
 
+// Shows several facts of the number seven
 body.appendChild(document.createElement("hr"))
 ulFavNum = document.createElement('ul')
-body.appendChild(ul)
+body.appendChild(ulFavNum)
 
 const promisesArr = []
 
@@ -34,8 +36,47 @@ for(let i = 0; i < 4; i++) {
     promisesArr.push(axios.get(`http://numbersapi.com/7/trivia?json`))
 }
 
-Promise.race(promisesArr)
+Promise.all(promisesArr)
 .then(nums => (nums.forEach(p => {
     let li = document.createElement('li')
-
+    li.innerHTML = `<strong style='font-size: 20px;'>${p.data.number}:</strong> ${p.data.text}`
+    ulFavNum.appendChild(li)
 })))
+
+
+// Part 2 Deck of Cards
+
+// const btn = document.createElement('button')
+// btn.classList.add("btn")
+// body.appendChild(btn)
+// const btnGrab = document.querySelector(".btn")
+
+// function pickCard(res) {
+//     if(res.data.remaining !== 0) {
+//         return axios.get(`http://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw`)
+//         .then(res => {
+//             console.log(res.data)
+//         })
+//     }
+// }
+
+// btnGrab.addEventListener('click', pickCard)
+
+let deckId = ''
+const deckPromise = axios.get("http://deckofcardsapi.com/api/deck/new/")
+.then(res => {
+    return axios.get(`http://deckofcardsapi.com/api/deck/${res.data.deck_id}/shuffle/`)
+})
+
+
+
+
+
+function pickCard() {
+
+    return new Promise((resolve, reject) => {
+        axios.get("http://deckofcardsapi.com/api/deck/new/")
+        .then(data => resolve(data.data.deck_id))
+        
+    });
+}
