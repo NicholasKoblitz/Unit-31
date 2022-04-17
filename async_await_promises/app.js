78776c5d190f02b0b8dc638c88350df485b53725
Nick <nick@Sabrinas-MacBook-Pro-4.local>
2022-04-17 12:57:56 -0400

    Finished 31.3 practice

--- async_await_promises/app.js
+++ async_await_promises/app.js
@@ -26,11 +26,11 @@ async function numsInfoPage() {
         axios.get("http://numberapi.com/random/trivia?json"),
         axios.get("http://numberapi.com/random/trivia?json")
     ]);
-    for(let num of numsList){
-        let li = document.createElement('li');
-        li.innerText = `${num.data.number}: ${num.data.text}`
-        ul.appendChild(li)
-    }
+    // for(let num of numsList){
+    //     let li = document.createElement('li');
+    //     li.innerText = `${num.data.number}: ${num.data.text}`
+    //     ul.appendChild(li)
+    // }
 }
 
 numsInfoPage()
@@ -56,7 +56,41 @@ const deck = {
     async drawCard() {
         let card = await axios.get(`http://deckofcardsapi.com/api/deck/${this.deckId}/draw`)
         console.log(card.data.cards[0].suit, card.data.cards[0].value)
-        console.log(card.data.remaining)
     }
 }
+
+
+body = document.querySelector("body");
+btn = document.createElement("button");
+btn.innerText = 'Pick Card'
+cardArea = document.createElement('div')
+cardArea.hidden = true;
+btn.classList.add("btn")
+body.appendChild(btn);
+body.appendChild(cardArea)
+
+let deckId = null;
+
+async function getDeck() {
+        let deck = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle");
+        deckId = deck.data.deck_id
+    }
+
+async function drawCard() {
+        let card = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw`)
+
+        if(card.data.remaining !== 0) {
+            let img = document.createElement('img')
+            img.src = card.data.cards[0].image
+            img.classList = "card"
+            body.appendChild(img)
+        }
+        else {
+            cardArea.innerText = 'No more cards'
+            cardArea.hidden = false
+        }
+}
+
+document.addEventListener('DOMContentLoaded', getDeck)
+btn.addEventListener('click', drawCard)
 
